de29fe9b98c2fa390a764ff58348c79746dac35d
edimilsonbraz <edimilson.gt8@gmail.com>
2020-06-28 14:46:47 -0300

    falta resolver o course

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ nunjucks.configure("views", {
     express:server,
     autoescape: false,
     noCache: true
-    
 })
 
-server.get("/", function(req, res) {  //req = request  res = response
+server.get("/", function(req, res) {
+    return res.render("courses", {items: courses })
+})
+
+server.get("/about", function(req, res) {  
     const about = {
         logo_url: "https://scontent.ffln4-1.fna.fbcdn.net/v/t1.0-9/104169738_687672685112542_1468337677738709328_n.png?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=bEpYvUGdhmwAX84vBlb&_nc_ht=scontent.ffln4-1.fna&oh=42865f744bc76dd27f654157a3b4f830&oe=5F106F73",
         name: "Rocketseat",
@@ -28,16 +31,16 @@ server.get("/", function(req, res) {  //req = request  res = response
         ]
     }
 
-    return res.render("about", {about: about}) //Primeira rota que é a padrao
+    return res.render("about", { about}) 
 })
 
-server.get("/courses", function(req, res) {  //req = request  res = response
+server.get("/course", function(req, res) {  
     
-    return res.render("courses", {items: courses}) //Segunda rota
+    return res.render("course", {course}) 
 })
 
 server.get("/courses/:id", function(req, res) {
-    const id = req.params.id;
+    const id = req.params.id
 
     const course = courses.find(function (course) {
         return course.id == id
